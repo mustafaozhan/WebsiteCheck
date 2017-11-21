89918ad942d937b51559c2b0f7483f8031fee006
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-11-21 13:21:37 +0300

    Fragment Changing implemented

--- app/src/main/java/mustafaozhan/github/com/websitecheck/activities/MainActivity.kt
+++ app/src/main/java/mustafaozhan/github/com/websitecheck/activities/MainActivity.kt
@@ -1,23 +1,29 @@
 package mustafaozhan.github.com.websitecheck.activities
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 import mustafaozhan.github.com.websitecheck.R
+import mustafaozhan.github.com.websitecheck.fragments.MainFragment
 import mustafaozhan.github.com.websitecheck.fragments.SettingsFragment
-import android.webkit.WebViewFragment
-
 
 
 class MainActivity : AppCompatActivity() {
+    private val MAIN = "main_fragment"
+    private val SETTINGS = "settings_fragment"
+    private var doubleBackToExitPressedOnce = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        fragmentManager.beginTransaction()
+                .add(R.id.frameLayout, MainFragment(), MAIN).commit()
         setListeners()
 
     }
@@ -27,7 +33,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addNewWebsite() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -42,15 +48,33 @@ class MainActivity : AppCompatActivity() {
             }
 
     private fun changeFragment(): Boolean {
-        if (supportFragmentManager.findFragmentById(R.id.fragment) != null) {//checking if any fragment is open
+        if (supportFragmentManager.findFragmentById(R.id.frameLayout) != null) {//checking if any fragment is open
             supportFragmentManager
                     .beginTransaction().
-                    remove(supportFragmentManager.findFragmentById(R.id.fragment)).commit()
+                    remove(supportFragmentManager.findFragmentById(R.id.frameLayout)).commit()
         }
         fragmentManager
                 .beginTransaction()
-                .replace(R.id.fragment, SettingsFragment())//opening preference fragment
+                .replace(R.id.frameLayout, SettingsFragment(), SETTINGS)//opening preference fragment
                 .commit()
         return true
+    }
+
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+            return
+        }
+        val myFragment = fragmentManager.findFragmentByTag(SETTINGS)
+        if (myFragment != null && myFragment.isVisible) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(R.id.frameLayout, MainFragment(), MAIN)
+                    .commit()
+        } else {
+            this.doubleBackToExitPressedOnce = true
+            Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show()
+            Handler().postDelayed({ doubleBackToExitPressedOnce = false }, 2000)
+        }
     }
 }
--- app/src/main/java/mustafaozhan/github/com/websitecheck/fragments/SettingsFragment.kt
+++ app/src/main/java/mustafaozhan/github/com/websitecheck/fragments/SettingsFragment.kt
@@ -2,6 +2,7 @@ package mustafaozhan.github.com.websitecheck.fragments
 
 import android.app.Fragment
 import android.os.Bundle
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -12,12 +13,8 @@ import mustafaozhan.github.com.websitecheck.R
  * Created by Mustafa Ozhan on 11/20/17 at 10:48 PM on Arch Linux.
  */
 class SettingsFragment : Fragment() {
-
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
-        Toast.makeText(view.context, "SettingsFragment", Toast.LENGTH_SHORT).show()
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Toast.makeText(inflater.context, "SettingsFragment", Toast.LENGTH_SHORT).show()
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
-
-
 }
