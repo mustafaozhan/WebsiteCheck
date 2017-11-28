c573ed4cbe816d72659d247b1299638b3b4e8bba
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-11-28 19:11:37 +0300

    Alarm Manager Implementing

--- app/src/main/java/mustafaozhan/github/com/websitecheck/utils/AlarmReceiver.kt
+++ app/src/main/java/mustafaozhan/github/com/websitecheck/utils/AlarmReceiver.kt
@@ -6,15 +6,25 @@ import android.content.Intent
 import android.media.MediaPlayer
 import android.support.v4.media.app.NotificationCompat
 import mustafaozhan.github.com.websitecheck.R
+import android.media.RingtoneManager
+import android.media.Ringtone
+import android.net.Uri
+import android.widget.Toast
+
 
 /**
  * Created by Mustafa Ozhan on 11/27/17 at 12:28 AM on Arch Linux.
  */
 class AlarmReceiver : BroadcastReceiver() {
-    private var mediaPlayer: MediaPlayer? = null
     override fun onReceive(context: Context?, intent: Intent?) {
-//        mediaPlayer = MediaPlayer.create(context, R.raw.alarm)
-//        mediaPlayer!!.start()
+
+        Toast.makeText(context, "Alarm !", Toast.LENGTH_LONG).show()
+        var alarmUri: Uri? = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        }
+        val ringtone = RingtoneManager.getRingtone(context, alarmUri)
+        ringtone.play()
 
     }
 
