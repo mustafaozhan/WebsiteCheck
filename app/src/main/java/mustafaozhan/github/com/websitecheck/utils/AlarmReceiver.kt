a9a2168225d52fd08092db96f01fc68b8acac93c
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-12-03 14:23:57 +0300

    Unique alarms and checking website is implemented

--- app/src/main/java/mustafaozhan/github/com/websitecheck/utils/AlarmReceiver.kt
+++ app/src/main/java/mustafaozhan/github/com/websitecheck/utils/AlarmReceiver.kt
@@ -23,37 +23,25 @@ import java.util.*
  */
 class AlarmReceiver : BroadcastReceiver() {
     companion object {
-
-
-        private val IS_ONE_TIME = "onetime"
         private val TEXT = "text"
     }
 
     override fun onReceive(context: Context, intent: Intent) {
         val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
         val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG")
-        //Acquire the lock
         wakeLock.acquire()
-
-        //You can do the processing here.
         val extras = intent.extras
         val stringBuilder = StringBuilder()
 
-        if (extras != null && extras.getBoolean(IS_ONE_TIME, false))
-            stringBuilder.append("One time Timer : ")
-
-        if (extras != null && extras.getString(TEXT, "").isEmpty())
-            stringBuilder.append("Nameless")
+        val text: String
+        text = if (extras != null && extras.getString(TEXT, "").isEmpty())
+            "Nameless"
         else
-            stringBuilder.append(extras.getString(TEXT))
-        Log.d("Alarm ", extras.getString(TEXT))
+            extras.getString(TEXT)
 
-        val simpleDateFormat = SimpleDateFormat("hh:mm:ss a")
-        stringBuilder.append(simpleDateFormat.format(Date()))
+        checkURL(text)
 
-        Toast.makeText(context, stringBuilder, Toast.LENGTH_LONG).show()
 
-        //Release the lock
         wakeLock.release()
     }
 
@@ -67,7 +55,6 @@ class AlarmReceiver : BroadcastReceiver() {
 
         val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val intent = Intent(context, AlarmReceiver::class.java)
-        intent.putExtra(IS_ONE_TIME, false)
         intent.putExtra(TEXT, item.name)
         val pendingIntent = PendingIntent.getBroadcast(context, item.requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT)
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), (1000 * 60 * item.period * temp).toLong(), pendingIntent)
@@ -80,7 +67,7 @@ class AlarmReceiver : BroadcastReceiver() {
         alarmManager.cancel(pendingIntent)
     }
 
-    fun checkURL(myURL: String) {
+    private fun checkURL(myURL: String) {
         doAsync {
             val url = URL(myURL)
             val connection = url.openConnection() as HttpURLConnection
@@ -88,7 +75,10 @@ class AlarmReceiver : BroadcastReceiver() {
             connection.connect()
             val code = connection.responseCode
             run {
-                //                Toast.makeText(, code.toString(), Toast.LENGTH_SHORT).show()
+                if (code.toString().startsWith("2"))
+                    Log.d("ONLINE:", myURL)
+                else
+                    Log.d("OFFLINE:", myURL)
             }
         }
 
