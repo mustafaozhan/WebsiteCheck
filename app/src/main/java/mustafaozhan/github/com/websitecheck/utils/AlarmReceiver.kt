32a1d0bbd8a4b7fc32c1f4238cbf4b2e3f8e21fe
Mustafa Ozhan <mr.mustafa.ozhan@gmail.com>
2017-12-03 14:56:48 +0300

    Libraries Updated

--- app/src/main/java/mustafaozhan/github/com/websitecheck/utils/AlarmReceiver.kt
+++ app/src/main/java/mustafaozhan/github/com/websitecheck/utils/AlarmReceiver.kt
@@ -1,37 +1,40 @@
 package mustafaozhan.github.com.websitecheck.utils
 
 
+import android.annotation.SuppressLint
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 import android.app.AlarmManager
 import android.app.PendingIntent
-import android.content.ClipData
 import android.os.PowerManager
 import android.util.Log
 import mustafaozhan.github.com.websitecheck.model.Item
 import org.jetbrains.anko.doAsync
 import java.net.HttpURLConnection
 import java.net.URL
-import java.text.SimpleDateFormat
-import java.util.*
+import android.os.Build
+import android.support.annotation.RequiresApi
+import android.support.v4.app.NotificationCompat
+import mustafaozhan.github.com.websitecheck.R
 
 
 /**
  * Created by Mustafa Ozhan on 11/27/17 at 12:28 AM on Arch Linux.
  */
+
+@RequiresApi(Build.VERSION_CODES.O)
 class AlarmReceiver : BroadcastReceiver() {
     companion object {
         private val TEXT = "text"
     }
 
+    @SuppressLint("WakelockTimeout")
     override fun onReceive(context: Context, intent: Intent) {
         val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
         val wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG")
         wakeLock.acquire()
         val extras = intent.extras
-        val stringBuilder = StringBuilder()
 
         val text: String
         text = if (extras != null && extras.getString(TEXT, "").isEmpty())
@@ -39,7 +42,7 @@ class AlarmReceiver : BroadcastReceiver() {
         else
             extras.getString(TEXT)
 
-        checkURL(text)
+        checkURL(text, context)
 
 
         wakeLock.release()
@@ -67,7 +70,7 @@ class AlarmReceiver : BroadcastReceiver() {
         alarmManager.cancel(pendingIntent)
     }
 
-    private fun checkURL(myURL: String) {
+    private fun checkURL(myURL: String, context: Context) {
         doAsync {
             val url = URL(myURL)
             val connection = url.openConnection() as HttpURLConnection
@@ -76,12 +79,22 @@ class AlarmReceiver : BroadcastReceiver() {
             val code = connection.responseCode
             run {
                 if (code.toString().startsWith("2"))
-                    Log.d("ONLINE:", myURL)
+                    senNotification(myURL, context)
                 else
                     Log.d("OFFLINE:", myURL)
             }
         }
 
+
+    }
+
+    @Suppress("DEPRECATION")
+    private fun senNotification(name: String, context: Context) {
+        Log.d("Whatsssup:", name)
+        val mBuilder = NotificationCompat.Builder(context)
+        mBuilder.setSmallIcon(R.drawable.ic_launcher_foreground)
+        mBuilder.setContentTitle(name)
+        mBuilder.setContentText("Online!")
 
     }
 }
